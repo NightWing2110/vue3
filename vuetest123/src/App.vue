f0a2de0d36aa919c41b88980a35ee0e2bc2ebe74
Duy <duylqh@adgad-net.vn>
2022-10-03 08:06:38 +0700

    vuecli

---
+++ vuetest123/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <navigation-component/>
+</div>
+</template>
+
+<script>
+import NavigationComponent from './components/NavigationComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavigationComponent
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    onChange()
+    {
+      console.log('Running');
+      console.log(this.$refs.hello);
+      console.log(this.$refs.header);
+      this.$refs.hello.onConsoleTesting();
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ vuetest123/src/components/NavigationComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <ul>
+      <li @click="changeHere">Home</li>
+      <li @click="changeHere">Band</li>
+      <li @click="changeHere">Tour</li>
+      <li @click="changeHere">Contact</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavigationComponent",
+  data() {
+    return {};
+  },
+
+  methods: {
+    changeHere() {
+      console.log("aaa");
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  width: 100%;
+  height: 52px;
+  border: 1px solid blue;
+  margin: 0;
+  padding: 0;
+}
+.container ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+}
+.container ul li {
+  margin: 0 30px;
+  cursor: pointer;
+}
+</style>
