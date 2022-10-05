38afad3e7e49db1b8900a16c077455e8382b75d8
Duy <duylqh@adgad-net.vn>
2022-10-05 09:29:06 +0700

    ref-reactive

---
+++ compositionapi/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <HomeComponent />
+</template>
+
+<script>
+import homeComponent from './pages/homeComponent.vue';
+import HomeComponent from './pages/homeComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    homeComponent,
+    HomeComponent
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ compositionapi/src/pages/homeComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <h1>Overview page is here</h1>
+    <p>{{ secondName }}</p>
+    <p>{{ car }}</p>
+    <button @click="onChangeSomething">CLick me</button>
+</template>
+
+<script>
+import { ref, reactive } from "vue"
+export default {
+    setup() {
+        const firstName = ref("NightWing");
+        const secondName = ref({
+            name: "Hu",
+            something: "Ha"
+        });
+        let car = reactive({
+            price: 2110,
+            name: "Audi"
+
+        });
+        
+        function onChangeSomething()
+        {
+            // console.log("Running..............");
+            // // firstName = "Alazul";
+            // console.log(secondName);
+            // console.log(car);
+            // firstName.value = "Alazul";
+            // secondName.value.name = "fffffff";
+            // car.price = 1999;
+            secondName.value = {
+                name: "Alazul",
+                something: "batcave"
+            };
+            car = {
+                price: 1999,
+                name: "something"
+            }
+        }
+
+        return { car, firstName, secondName, onChangeSomething };
+    }
+};
+</script>
